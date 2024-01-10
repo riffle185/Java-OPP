03c5ad2278f3778d797e8ee0e6d336265ac7a518
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2024-01-10 20:58:31 +0200

    Update Main.java

--- Working with Abstraction/Working with Abstraction - Exercise/CardsWithPower/Main.java
+++ Working with Abstraction/Working with Abstraction - Exercise/CardsWithPower/Main.java
@@ -5,13 +5,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         String card = scanner.nextLine();
         String suit = scanner.nextLine();
 
         CardName cardName1 = CardName.valueOf(card);
         CardType cardType1 = CardType.valueOf(suit);
-
         System.out.printf("Card name: %s of %s; Card power: %d"
                 , card, suit, cardName1.getPower() + cardType1.getValue());
     }
